3a9009ad55d0399706fc006e920c14a7208c5134
patrick-andrade-flausino <patrickandradeflausino@gmail.com>
2024-04-19 18:06:58 -0300

    feat: projeto portfolio finalizado

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,4 +1,5 @@
 import { SectionTitle } from "../sectionTitle/sectionTitle";
+import Image from "next/image";
 
 import "./experience.scss";
 
@@ -10,7 +11,56 @@ export function Experience() {
         3 years working as a Software Developer, in companys such as
         Rendimento/Pay, BrasilCard and A2W.
       </p>
-      <div className="experience-time"></div>
+      <div className="experience-time">
+        <div className="componenteImagem">
+          <Image
+            src="/react.svg"
+            alt="Next.js Logo"
+            width={36}
+            height={34}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure-1">1 year</div>
+          </div>
+        </div>
+        <div className="componenteImagem">
+          <Image
+            src="/js.svg"
+            alt="Next.js Logo"
+            width={36}
+            height={34}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure-2">2 year</div>
+          </div>
+        </div>
+        <div className="componenteImagem">
+          <Image
+            src="/ts.svg"
+            alt="Next.js Logo"
+            width={36}
+            height={34}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure-2">2 year</div>
+          </div>
+        </div>
+        <div className="componenteImagem">
+          <Image
+            src="/java.svg"
+            alt="Next.js Logo"
+            width={36}
+            height={34}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure-3">3 year</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
